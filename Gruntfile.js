d384c790d3cff5c1684140e473ab7ac9223468b3
Scott <scott@sscotth.io>
2015-02-23 14:59:26 -0600

    copy grunt task

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  grunt.initConfig({
+    copy: {
+      main: {
+        files: [
+          {expand: true, cwd: 'app/', src: ['**'], dest: 'public/', filter: 'isFile'}
+        ]
+      }
+    }
+  });
+
+  grunt.registerTask('default', []);
+};
